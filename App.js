062950565f4c418d3272b8beba786777e2941593
Lucas Angelo <lucasg258@gmail.com>
2021-03-17 10:33:39 -0300

    Calculadora feita e erros corrigidos

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
@@ -12,53 +12,53 @@ const initialState = {
 };
 
 export default class App extends Component {
-  state = {...initialState};
+  state = { ...initialState };
 
   addDigit = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
-      return
-    }
-
     const clearDisplay =
       this.state.displayValue === '0' || this.state.clearDisplay;
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+      return;
+    }
+
     const currentValue = clearDisplay ? '' : this.state.displayValue;
     const displayValue = currentValue + n;
-    this.setState({displayValue, clearDisplay: false});
+    this.setState({ displayValue, clearDisplay: false });
 
     if (n !== '.') {
       const newValue = parseFloat(displayValue);
       const values = [...this.state.values];
       values[this.state.current] = newValue;
-      this.setState({values});
+      this.setState({ values });
     }
   };
 
   clearMemory = n => {
-    this.setState({...initialState});
+    this.setState({ ...initialState });
   };
 
   setOperation = operation => {
-    if(this.state.current === 0){
-      this.setState({ operation, current: 1, clearDisplay: true })
-    }else {
-      const equals = operation === '='
-      const values = [...this.state.values]
-      try{
-        values[0] = 
-        eval (`${values[0]} ${this.state.operation} ${values[1]}`)
-      }catch(e){
-        values[0] = this.state.values[0]
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
       }
 
-      values[1] = 0
+      values[1] = 0;
       this.setState({
-        displayValue: values[0],
-        operation: equals ? null : operation, 
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
         current: equals ? 0 : 1,
         // clearDisplay: !equals,
         clearDisplay: true,
         values,
-      })
+      });
     }
   };
 
